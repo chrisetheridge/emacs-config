28c069e5e371ddfc029771c5b6107e8d7d860aff
Chris Etheridge <chris.etheridge@cognician.com>
2018-08-01 18:22:42 +0200

    Init config

---
+++ init.el
@@ -0,0 +1,25 @@
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(require 'package)
+(package-initialize)
+
+(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
+    (load-file (expand-file-name "config.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (aggressive-indent company nord-theme smex use-package-ensure-system-package diminish delight use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
