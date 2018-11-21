8ffd4e1ef68706c6b6458af73b33714370bf7115
Chris Etheridge <chrise@Chriss-MacBook-Pro.local>
2018-11-21 13:35:10 +0200

    Fix initial set up code

--- init.el
+++ init.el
@@ -3,6 +3,21 @@
 (require 'package)
 (package-initialize)
 
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  ;; org mode
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+      
+
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
@@ -16,7 +31,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (aggressive-indent company nord-theme smex use-package-ensure-system-package diminish delight use-package))))
+    (rainbow-mode rainbow-delimiters org-plus-contrib undo-tree iedit yasnippet-snippets which-key wakatime-mode use-package persp-projectile ivy-yasnippet highlight-symbol flycheck diminish cider ag ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
